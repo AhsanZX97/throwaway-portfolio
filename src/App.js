1aa4d59e410bd1c31d0f821408fffa58327ce696
Ahsan Zia <ahsan97@hotmail.co.uk>
2019-08-31 20:21:58 +0100

    turned the template into components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Contact from './components/contact';
+import Navigation from './components/navigation';
+import About from './components/about';
+import Copyright from './components/copyright';
+import Footer from './components/footer';
+import Masthead from './components/masthead';
+import Modal from './components/modal';
+import Portfolio from './components/portfolio';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <Masthead />
+      <About />
+      <Portfolio />
+      <Contact />
+      <Footer />
+      <Copyright />
+      <Modal />
     </div>
   );
 }
